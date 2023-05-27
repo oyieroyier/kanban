6c203da63da47f354705536dc97b9aeeeb956ba6
Bob Oyier <boboyier92@gmail.com>
2023-05-27 23:14:16 +0300

    Writing tests 😭 😭

---
+++ src/store/store.test.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useStore } from './store';
+import { render } from '@testing-library/react';
+
+function TestComponent({ selector, effect }) {
+	const items = useStore(selector);
+
+	useEffect(() => effect(items), [items]);
+
+	return null;
+}
+
+test('should return default value at the start', () => {
+	const selector = (store) => store.tasks;
+	const effect = vi.fn();
+
+	render(<TestComponent selector={selector} effect={effect} />);
+	expect(effect).toHaveBeenCalledWith([]);
+});
+
+test('should add  item to the store ad rerun the effect', () => {
+	const selector = (store) => ({ tasks: store.tasks, addTask: store.addTask });
+	const effect = vi.fn().mockImplementation((items) => {
+		if (!items.tasks.length) items.addTask('a', 'b');
+	});
+
+	render(<TestComponent selector={selector} effect={effect} />);
+	expect(effect).toHaveBeenCalledTimes(2);
+	expect(effect).toHaveBeenCalledWith(
+		expect.objectContaining({ tasks: [{ title: 'a', state: 'b' }] })
+	);
+});
